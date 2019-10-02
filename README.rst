47faca429a8cc4d578b3b5c42e0bf3237ac42326
Alex Tarr <atarr3@gmail.com>
2019-10-02 13:43:25 -0400

    first up

---
+++ README.rst
@@ -0,0 +1,5 @@
+=========
+campvideo
+=========
+
+Python package for political campaign advertisement analysis
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='campvideo',
+    version='0.1.0',
+    description='Analyzes political campaign advertisements.',
+    long_description=open('README.rst').read(),
+    author='Alex Tarr',
+    author_email='atarr3@gmail.com',
+	url='https://github.com/atarr3/campvideo',
+    packages=['campvideo'],
+    scripts=['bin/audio_feats.py','bin/match_vids','bin/summarize_vids.py'],
+    license='LICENSE',
+    install_requires=[
+        "ffmpeg-python",
+		"numpy",
+		"opencv-python >= 3.4.2",
+		"pandas >= 0.25"
+		"scikit-learn >= 0.20.1",
+		"scipy >= 1.1.0"
+    ]
+)
