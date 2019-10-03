9998313db78e4b7787b7c07d9d600ebda06b5c0a
Alex Tarr <atarr3@gmail.com>
2019-10-03 13:24:00 -0400

    updated version requirements in package dependencies

--- setup.py
+++ setup.py
@@ -9,13 +9,15 @@ setup(
     author_email='atarr3@gmail.com',
     url='https://github.com/atarr3/campvideo',
     packages=['campvideo'],
-    scripts=['bin/audio_feats.py','bin/match_vids.py','bin/summarize_vids.py'],
+    scripts=['bin/audio_feats.py',
+		     'bin/match_vids.py',
+			 'bin/summarize_vids.py'],
     license='MIT License',
     install_requires=[
-        	"ffmpeg-python",
+		"ffmpeg-python",
 		"numpy",
-		"opencv-python >= 3.4.2",
-		"pandas >= 0.25",
+		"opencv-python >= 4.1.1.26",
+		"pandas >= 0.25.1",
 		"scikit-learn >= 0.20.1",
 		"scipy >= 1.1.0"
     ]
