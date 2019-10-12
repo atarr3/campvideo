044e7ecb1224fbf8ee665e15b048e578bdf679ee
Alex Tarr <atarr3@gmail.com>
2019-10-12 17:49:17 -0400

    updated install instructions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='campvideo',
-    version='0.2.6',
+    version='0.2.7',
     description='Analyzes political campaign advertisements.',
     long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
     author='Alex Tarr',
