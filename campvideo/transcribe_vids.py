2acb8b80d5bfeebc55269724f57bac1dde7b29cc
Alex Tarr <atarr@dhcp-10-250-190-13.harvard.edu>
2019-11-26 18:10:33 -0500

    updates to transcribe_vids script

--- campvideo/transcribe_vids.py
+++ campvideo/transcribe_vids.py
@@ -19,7 +19,7 @@ STATES = {"AL":"Alabama", "AK":"Alaska","AR":"Arkansas","AZ":"Arizona","CA":"Cal
           "SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas",
           "UT":"Utah","VT":"Vermont","VA":"Virginia","WA":"Washington",
           "WV":"West Virginia","WI":"Wisconsin","WY":"Wyoming","US":"United States"}
-          
+
 ELECTS = {'sen':'Senate','hou':'House','gov':'Gubernatorial','pre':'Presidential'}
 
 # returns list of videos in given directory which have a matching entry in WMP data
@@ -28,9 +28,9 @@ def _matched_vids(vid_paths,wmp_file):
     wmp = pd.read_csv(wmp_file,usecols=['uid']
                      ).drop_duplicates(subset='uid')
     ids = set(wmp.uid)
-    
+
     return [fpath for fpath in vid_paths if splitext(basename(fpath))[0] in ids]
-    
+
 def _get_metadata(fpath):
     # get election/year and state/name
     elye,stna = fpath.split('\\')[-3:-1]
@@ -44,114 +44,51 @@ def _get_metadata(fpath):
          name = stna[5:]
     else:
         name = stna[3:]
-    
+
     return elect,year,state,name
-    
+
 def _build_context(df):
     dem = df.iloc[0]['D'].split(',')
     rep = df.iloc[0]['R'].split(',')
     thi = df.iloc[0]['T'].split(',')
-    
+
     if dem != '' and rep != '':
         context = list(filter(None,dem+rep))
     else:
         # only retain first third-party candidate
         context = list(filter(None,dem+rep+thi[0]))
-        
+
     return context
-    
-# transcription via google cloud
-def transcribe(vid_path,context=None):
-    # instantiate storage client and video client
-    storage_client = storage.Client('adclass-1286')
-    video_client = vi.VideoIntelligenceServiceClient()
-    
-    # get video files bucket
-    bucket = storage_client.get_bucket('video_files')
-    
-    # upload file to bucket
-    blob = bucket.blob(splitext(basename(vid_path))[0])
-    blob.upload_from_filename(vid_path)
-    
-    # speech context
-    if context is not None:
-        sc = vi.types.SpeechContext(phrases=context)
-    
-        # transcription configuration
-        stc = vi.types.SpeechTranscriptionConfig(
-                                        language_code='en-US',
-                                        enable_automatic_punctuation=False,
-                                        speech_contexts=[sc]
-                                                )
-    else:
-        # transcription configuration
-        stc = vi.types.SpeechTranscriptionConfig(
-                                        language_code='en-US',
-                                        enable_automatic_punctuation=False
-                                                )
-    
-    video_context = vi.types.VideoContext(speech_transcription_config=stc)
-    
-    # construct video intelligence client request
-    video_gcs = 'gs://video_files/' + blob.name
-    features = [vi.enums.Feature.SPEECH_TRANSCRIPTION]
-    operation = video_client.annotate_video(video_gcs, 
-                                            features=features,
-                                            video_context=video_context,
-                                           )
-        
-    # get results and delete object from bucket
-    try:
-        results = operation.result(timeout=600)
-    except KeyboardInterrupt:
-        blob.delete()
-    finally:
-        blob.delete()
-    
-    # print out transcription
-    annotation_results = results.annotation_results[0]
-    speech_transcription = annotation_results.speech_transcriptions[0]
-    alternative = speech_transcription.alternatives[0]
-    
-    return alternative.transcript
-    
 
-def _parse_arguments(argv):
+def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('vid_dir', 
+    parser.add_argument('vid_dir',
                         help='Path to video file directory for transcription')
-    parser.add_argument('-wf','--wmp_file',default=None,
-                        help='Path to WMP data file for matching videos in the '
-                             'WMP file to those in vid_dir')
-    parser.add_argument('-nf','--names_file',default=None,
-                        help='Path to names data file for building speech context'
-                             ' from names')
+    parser.add_argument('-nf','--names-file',default='',
+                        help='Path to names data file for providing phrase hints'
+                             ' for speech transcription')
 
-    return parser.parse_args(argv)
-    
+    return parser.parse_args()
+
 if __name__ == '__main__':
     # get CL arguments
-    args = _parse_arguments(sys.argv[1:])
-    vid_dir,wmp,names = args.vid_dir,args.wmp_file,args.names_file
-    
+    args = parse_arguments()
+    vid_dir,names = args.vid_dir,args.names_file
+
     # get video paths
-    vid_paths = [join(root,fname) for root,fold,fnames in os.walk(vid_dir) 
-                 for fname in fnames if fname.endswith('.mp4')]               
-    # filter down to matched videos
-    if wmp is not None:
-        matched = _matched_vids(vid_paths,wmp)
-    else:
-        matched = vid_paths
-    nvids = len(matched)
-    
+    fpaths = [join(root,fname) for root,fold,fnames in os.walk(vid_dir)
+                                   for fname in fnames
+                                       if fname.endswith('.mp4')]
+    n_vids = len(fpaths)
+
     # open names file if given
     if names is not None:
-        names = pd.read_csv(names,keep_default_na=False,encoding='mbcs')   
-    
+        names = pd.read_csv(names,keep_default_na=False,encoding='mbcs')
+
     # output directory for transcripts (in root of vid_dir)
     if not exists(join(vid_dir,'transcripts')):
         os.mkdir(join(vid_dir,'transcripts'))
-        
+
     # transcribe
     for i,vid_path in enumerate(matched):
         print('Transcribing video {0} of {1}...'.format(i+1,nvids),end=' ',flush=True)
@@ -163,11 +100,11 @@ if __name__ == '__main__':
         if exists(join(vid_dir,'transcripts',cur_id+'.txt')):
             print('transcription already exists')
             continue
-            
+
         # get context
         if names is not None:
-            sub = names[(names.election == elect) & (names.year == int(year)) & 
-                        (names.state == state) & ((names.D.str.contains(cand)) | 
+            sub = names[(names.election == elect) & (names.year == int(year)) &
+                        (names.state == state) & ((names.D.str.contains(cand)) |
                         (names.R.str.contains(cand)) | (names['T'].str.contains(cand)))]
             try:
                 context = _build_context(sub)
