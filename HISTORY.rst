f298d1bde6ff75f4fe236a3dab6c636511d537df
Alex Tarr <atarr3@gmail.com>
2022-06-06 22:10:19 -0400

    package versioning changes

--- HISTORY.rst
+++ HISTORY.rst
@@ -1,5 +1,25 @@
 History
 =======
+1.2.3 (05/31/2022)
+------------------
+
+- Package version changes for Python 3.8.
+
+1.2.2 (05/31/2022)
+------------------
+
+- Added console script for downloading models.
+
+1.2.1 (05/31/2022)
+------------------
+
+- Package version bugfixes.
+
+1.2.0 (05/31/2022)
+------------------
+
+- Update package versions for Python 3.7 compatibility.
+
 1.1.0 (04/29/2022)
 ------------------
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='campvideo',
-    version='1.1.0',
+    version='1.2.3',
     description='Analyzes political campaign advertisements.',
     long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
     author='Alex Tarr',
@@ -14,28 +14,26 @@ setup(
 	},
     entry_points={
         'console_scripts': [
-            'audio_feats=campvideo.audio_feats:main',
-			'download_models=campvideo.download_models:main',
-			'imtext=campvideo.imtext:main',
-            'match_vids=campvideo.match_vids:main',
-            'summarize_vids=campvideo.summarize_vids:main',
-			'transcribe_vids=campvideo.transcribe_vids:main'
+            # 'audio_feats=campvideo.audio_feats:main',
+			'download_models=campvideo.download_models:main'
+			# 'imtext=campvideo.imtext:main',
+            # 'match_vids=campvideo.match_vids:main',
+            # 'summarize_vids=campvideo.summarize_vids:main',
+			# 'transcribe_vids=campvideo.transcribe_vids:main'
         ]
     },
     license='MIT License',
-	python_requires='>= 3.9',
+	python_requires='>=3.8,<3.11',
     install_requires=[
-		"face_recognition",
-		"ffmpeg-python",
-		"google-cloud-videointelligence>=2.7",
-		"google-cloud-vision>=2.7",
-        "matplotlib",
-        "seaborn>=0.11.0",
-		"numpy>=1.21",
-		"opencv-python>=4.5.5",
-		"pandas>=1.4",
+		"face_recognition==1.3.0",
+		"ffmpeg-python==0.2.0",
+		"google-cloud-videointelligence==2.7.0",
+		"google-cloud-vision==2.7.2",
+		"numpy==1.22.4",
+		"opencv-python==4.5.5.64",
+		"pandas==1.4.2",
 		"scikit-learn==1.0.1",
-		"scipy>=1.7",
+		"scipy==1.8.1",
         "spacy==3.3.0"
     ]
 )
